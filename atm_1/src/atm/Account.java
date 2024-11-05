8d24c19f0746ba8106e34dae9a468dc14ac9a823
Khankw <jskw247@gmail.com>
2024-11-05 16:09:58 +0900

    Update Account.java

--- atm_1/src/atm/Account.java
+++ atm_1/src/atm/Account.java
@@ -1,5 +1,24 @@
 package atm;
 
-public class Account {
+public class Account { // 각 유저가 갖는 복수의 계좌?
+	private int account;
+	private int money;
 
+	public void addMoney(int money) {
+		this.money += money;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Account) {
+			Account other = (Account) obj;
+		}
+
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("계좌 %d - 잔액 %d원", account, money);
+	}
 }
