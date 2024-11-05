ea592c6c06ee14c00c831d216ee964404550e99e
Khankw <jskw247@gmail.com>
2024-11-05 18:12:38 +0900

    Modify User

--- atm_1/src/atm/User.java
+++ atm_1/src/atm/User.java
@@ -3,47 +3,83 @@ package atm;
 import java.util.ArrayList;
 
 public class User {
-	private int code;
+	private int code; // 부여 ID?
 	private String name;
+	private String pw;
 	private ArrayList<Account> accounts;
-	
-	public User( int code, String name) {
-		this.code=code;
-		this.name=name;
+
+	public User(int code, String pw) {
+		this.code = code;
+		this.name = name;
+		this.pw = pw;
 	}
-	
-	public void addAccount(Account account) {
-		accounts.add(account);
+
+	public User(int code, String name, String pw) {
+		this.code = code;
+		this.name = name;
+		this.pw = pw;
 	}
-	
-	public void removeAccount(Account account) {
-		accounts.remove(account);
-	}
-	
-	public Account getAccountByIndex(int index) {
-		return accounts.get(index);
-	}
-	
-	public Account getAccount(int code) {
-		Account account=new Account(code);
-		
-		int idx=accounts.indexOf(account);
-		if(idx == -1)
-			return null;
-		
-		return accounts.get(idx);
-	}
-	
+
 	public int getCode() {
 		return this.code;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
+	public String getPassword() {
+		return this.pw;
+	}
+
+	public void addAccount(Account account) {
+		accounts.add(account);
+	}
+
+	public void removeAccount(Account account) {
+		accounts.remove(account);
+	}
+
+	public Account getAccountByIndex(int index) {
+		return accounts.get(index);
+	}
+
+	public Account getAccount(int code) {
+		Account account = new Account(code);
+
+		int idx = accounts.indexOf(account);
+		if (idx == -1)
+			return null;
+
+		return accounts.get(idx);
+	}
+
+	public int getAccountSize() {
+		return accounts.size();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof User) {
+			User other = (User) obj;
+			return other.code == this.code && other.pw.equals(other.pw);
+		}
+		return false;
+	}
+
 	@Override
 	public String toString() {
-		return this.code+") "+this.name;
+		String msg = String.format("%s님 계좌 목록\n", this.name);
+		int size = accounts.size();
+		for (int i = 0; i < size; i++) {
+			msg += String.format("%d) %s", i + 1, accounts.get(i));
+			if (i < size - 1)
+				msg += "\n";
+		}
+
+		if (size == 0)
+			msg += "만들어진 계좌가 없습니다.";
+
+		return msg;
 	}
 }
