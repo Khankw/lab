f9c07d38259cf90f8bdac6b3bf0927f88ca809e4
Khankw <jskw247@gmail.com>
2024-11-05 15:26:57 +0900

    Update User toString()

--- atm_1/src/atm/User.java
+++ atm_1/src/atm/User.java
@@ -8,4 +8,22 @@ public class User {
 	private String name;
 	private ArrayList<Account> accounts;
 	
+	public User( int code, String name) {
+		this.code=code;
+		this.name=name;
+		
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.code+") "+this.name;
+	}
 }
