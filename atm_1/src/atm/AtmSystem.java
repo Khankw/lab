ebf431e4cd4fa7789abf0ee632715ffe426e3c02
Khankw <jskw247@gmail.com>
2024-11-05 18:24:05 +0900

    Modify AtmSystem users - 초기화

--- atm_1/src/atm/AtmSystem.java
+++ atm_1/src/atm/AtmSystem.java
@@ -18,25 +18,36 @@ public class AtmSystem {
 	private Scanner scan = new Scanner(System.in);
 	private Random ran = new Random();
 
-	private ArrayList<User> users;
+	private ArrayList<User> users = new ArrayList<>();
 	private int log = -1;
 
 	public void run() {
 		// 회원가입/탈퇴
 		// 계좌신청/ 철회 (1인 3계좌까지)
 		// 로그인
-		printMenu();
-		runMenu();
+		while (true) {
+			printMenu();
+			runMenu();
+		}
 	}
 
 	private void printMenu() {
+		printUsersInfo();
 		System.out.println("1)회원가입");
 		System.out.println("2)회원탈퇴");
 		System.out.println("3)계좌신청");
 		System.out.println("4)계좌철회");
 		System.out.println("5)로그인");
+		System.out.println("6)로그아웃");
 	}
 
+	private void printUsersInfo() {
+		for(int i=0;i<users.size();i++) {
+			System.out.println(users.get(i));
+		}
+		
+	}
+	
 	private void runMenu() {
 		int sel = (int) input("메뉴 선택", NUM);
 
@@ -47,6 +58,7 @@ public class AtmSystem {
 			System.err.println("로그인 후 이용가능");
 			return;
 		}
+		System.out.println(log);
 
 		if (sel == JOIN)
 			join();
@@ -79,8 +91,9 @@ public class AtmSystem {
 	private int generateUserCode() {
 		while (true) {
 			int code = ran.nextInt(9000) + 1000;
-
+			System.out.println(code);
 			boolean isFound = false;
+			System.out.println(users.size());
 			for (int i = 0; i < users.size(); i++)
 				if (users.get(i).getCode() == code)
 					isFound = true;
@@ -141,16 +154,16 @@ public class AtmSystem {
 
 	private void closeAccount() {
 		User user = users.get(log);
-		int size=user.getAccountSize();
+		int size = user.getAccountSize();
 		if (size == 0) {
 			System.err.println("개설된 계좌가 없습니다.");
 			return;
 		}
 		System.out.println("====== 계좌철회 ======");
 		System.out.println(user);
-		int sel=(int) input("철회할 계좌 선택", NUM)-1;
-		
-		if(sel<0|| sel>=size) {
+		int sel = (int) input("철회할 계좌 선택", NUM) - 1;
+
+		if (sel < 0 || sel >= size) {
 			System.err.println("입력범위 오류");
 			return;
 		}
@@ -160,14 +173,15 @@ public class AtmSystem {
 
 	private void login() {
 		System.out.println("====== 로그인 ======");
-		
+
 		int code = (int) input("회원코드 입력", NUM);
 		String pw = (String) input("비밀번호 입력", STR);
-		
+
 		User user = new User(code, pw);
-		
+
 		log = users.indexOf(user);
-		if(log!=-1)
+		System.out.println(log);
+		if (log != -1)
 			System.out.println("로그인 완료");
 		else
 			System.err.println("회원정보를 확인해주세요.");
@@ -175,7 +189,7 @@ public class AtmSystem {
 
 	private void logout() {
 		System.out.println("====== 로그아웃 ======");
-		log=-1;
+		log = -1;
 		System.out.println("로그아웃 완료");
 	}
 
