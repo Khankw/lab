d5d099c00cb34210cd7b9ed1c1856e4e5f6b87a2
Khankw <jskw247@gmail.com>
2024-11-06 10:25:53 +0900

    Update Account.java variable money

--- atm_1/src/atm/Account.java
+++ atm_1/src/atm/Account.java
@@ -1,23 +1,28 @@
 package atm;
 
-public class Account { 
+public class Account {
 	private int code;
+	private int money;
 
 	public Account(int code) {
 		this.code = code;
+	}
+
+	public void addMoney(int money) {
+		this.money += money;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Account) {
 			Account other = (Account) obj;
-			return other.code==this.code;
+			return other.code == this.code;
 		}
 		return false;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("계좌 번호 - %d", code);
+		return String.format("계좌 %d - 잔액 : %d원", code, money);
 	}
 }
